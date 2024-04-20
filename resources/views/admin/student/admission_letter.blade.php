c2a54f7d401448ca81c44529d5c7494e7013527b
germanusK <germanuskeming@gmail.com>
2024-04-20 17:26:12 +0100

    debug attempt

--- resources/views/admin/student/admission_letter.blade.php
+++ resources/views/admin/student/admission_letter.blade.php
@@ -58,7 +58,17 @@
                             <b>@lang('text.offer_of_admission')</b>
                         </div>
                         <div class="my-1 py-1">
-                            <span>@lang('text.admission_letter_text_block1', ['degree'=>$degree??'DEG', 'department'=>$fee['department']??'DEP', 'year'=>$year??'NO-YEAR', 'tution_fee'=>$fee['amount']??'FEE', 'first_installment'=>$fee['first_installment']??'1INST', 'reg_fee'=>$fee['registration']??'RG.FEE', 'fee_total'=>($fee['amount']+$fee['registration']??0), 'international'=>$fee['international_amount']])</span>
+                            <span>@lang('text.admission_letter_text_block1', [
+                                    'degree'=>($degree??'DEG'), 
+                                    'department'=>($fee['department']??'DEP'), 
+                                    'year'=>($year??'NO-YEAR'), 
+                                    'tution_fee'=>($fee['amount']??'FEE'), 
+                                    'first_installment'=>($fee['first_installment']??'1INST'), 
+                                    'reg_fee'=>($fee['registration']??'RG.FEE'), 
+                                    'fee_total'=>($fee['amount']??0)+($fee['registration']??0), 
+                                    'international'=>$fee['international_amount']
+                                ])
+                            </span>
                         </div>
                         <div class="my-1 py-1">
                             <span>@lang('text.the_tution_fee_amount_should_be_paid_at'):</span><br>
