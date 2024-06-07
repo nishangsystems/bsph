bc3b06650844ae54e61eb7c324dd26ebce4d775c
germanusK <germanuskeming@gmail.com>
2024-06-07 11:16:32 +0100

    --------------

--- app/Http/Controllers/Student/HomeController.php
+++ app/Http/Controllers/Student/HomeController.php
@@ -392,8 +392,8 @@ class HomeController extends Controller
                     if($resp_data->count() > 0 and $resp_data->first() != null){
                         return redirect(route('student.momo.processing', $resp_data->first()));
                     }else{
-                        return back()->with('error', $response->body())->with('message', $response->body());
-                        return back()->with('error', "Payment request failed")->with('message', $response->body());
+                        // return back()->with('error', $response->body());
+                        return back()->with('error', "Payment request failed");
                     }
                     break;
 
