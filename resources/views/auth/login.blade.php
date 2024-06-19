c492e24cbc7100ca4ea47be5b28fb7ececb02927
germanusK <germanuskeming@gmail.com>
2024-06-19 10:50:03 +0100

    ----

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -216,7 +216,7 @@
 										</div>
 										<div class="toolbar clearfix"  style="border: 0px; font-size: xsmall !important; width: 77% !important; margin-inline: auto; ">
 											<span style="text-decoration: none !important; color: black !important;">{{__('text.need_an_account?')}}</span>
-											<a href="{{ route('registration') }}" style="color: #670404 !important;">
+											<a href="{{ route('registration') }}" class="btn rounded-3 btn-sm btn-black" style="border-radius: 2rem; color: #670404 !important; background-color: white !important; border: 1px solid {{ $bg1 }} !important; font-weight: semibold !important;">
 												{{__('text.want_to_register')}}
 												{{-- <i class="ace-icon fa fa-arrow-right"></i> --}}
 												</a>
