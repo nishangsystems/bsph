e9b2ac5ee870da09ac010accc659a6acb7835066
germanusK <germanuskeming@gmail.com>
2024-07-31 13:32:55 +0100

    payment follower set

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('followupApplicationPayment')->hourly();
+        $schedule->command('followupApplicationPayment')->everyTenMinutes();
     }
 
     /**
